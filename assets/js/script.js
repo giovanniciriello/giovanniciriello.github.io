fa3bd66288631c44aca5dae158199699eb35faf4
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2020-11-30 01:53:58 +0100

    init website

---
+++ assets/js/script.js
@@ -0,0 +1,159 @@
+const workExperiences = [
+
+  {
+    role: 'Frontend Developer',
+    company: 'Datbrain LTD',
+    daterange: '03/2020 - Current',
+    place: 'London, England (Remote)',
+    works: [
+      {
+        description: 'Development of Smarkez, a business browser in a cross-platform desktop app. Working in a 15+ devs team.',
+        techs: ['Angular', 'Electron', 'Bootstrap']
+      },
+    ],
+  },
+
+  {
+    role: 'Full-Stack Developer',
+    company: 'Hagakure Restaurants Group',
+    daterange: '09/2017 - Current',
+    place: 'Bari, Italy (Remote)',
+    works: [
+      {
+        description: 'Design and development of cloud software in continuous evolution for the management of financial movements, work shifts, calculation of salaries, analysis of collected data.',
+        techs: ['Laravel', 'VueJS', 'Bootstrap']
+      },
+      {
+        description: 'Design and development of the Android app Digital Wine Showcase, currently running in production on more than 30 tablets, used as an electronic wine list in restaurants.',
+        techs: ['Angular', 'Ionic']
+      },
+      {
+        description: 'Development of corporate group websites, including e-commerce.',
+        techs: ['Laravel', 'Bootstrap', 'Prestashop']
+      },
+      {
+        description: 'Development of mail and Instagram automation tool, with web scraping for new customer acquisition and customer loyalty.',
+        techs: ['Python']
+      }
+    ],
+  },
+
+  {
+    role: 'Freelance Web Developer',
+    company: 'Burger King',
+    daterange: '06/2019 - 08/2019',
+    place: 'Bari, Italy (Remote)',
+    works: [
+      {
+        description: 'Design and development of the web app for the management of short-term products. It’s currently running in the kitchens of burger king branch in Bari, Italy.',
+        techs: ['Laravel', 'VueJS', 'Bootstrap']
+      },
+    ],
+  },
+
+  {
+    role: 'Freelance Laravel Master Coach',
+    company: 'iComm Lab',
+    daterange: '09/2018 - 10/2018',
+    place: 'Acquaviva d. Fonti, Italy',
+    works: [
+      {
+        description: 'Coach of the Laravel PHP framework for the company iCommLab\'s employees in a intensive on-site 32 hours course.',
+        techs: ['Laravel']
+      },
+    ],
+  },
+
+  {
+    role: 'Founder & Full-Stack developer',
+    company: 'Admap',
+    daterange: '01/2016 - 01/2017',
+    place: 'Rome, Italy',
+    works: [
+      {
+        description: 'Admap is a startup I founded in 2015. It is a platform that allows small and medium enterprises to create advertising campaigns (posters, billboards) through an online process and by consulting a special national map. The startup received rich funding from a company in Rome in February 2016 and was sold in January 2017.',
+        techs: ['Laravel', 'Python', 'Bootstrap']
+      },
+    ],
+  },
+]
+
+const skillCategories = [
+  {
+    name: 'Backend',
+    skills : [
+      { name: 'PHP', icon: 'PHP.svg' },
+      { name: 'Laravel', icon: 'Laravel.svg' },
+      { name: 'Python', icon: 'Python.svg' },
+      { name: 'Django', icon: 'Django.svg' },
+      { name: 'MySQL', icon: 'MySQL.svg' },
+      { name: 'MongoDB', icon: 'MongoDB.svg' }
+    ]
+  },
+  {
+    name: 'Frontend',
+    skills : [
+      { name : 'HTML', icon: 'HTML.svg' },
+      { name : 'CSS', icon: 'CSS.svg' },
+      { name : 'Sass', icon: 'Sass.svg' },
+      { name : 'Javascript', icon: 'Javascript.svg' },
+      { name : 'Typescript', icon: 'Typescript.svg' },
+      { name : 'Angular', icon: 'Angular.svg' },
+      { name : 'Electron', icon: 'Electron.svg' },
+      { name : 'Ionic', icon: 'Ionic.svg' },
+      { name : 'VueJS', icon: 'VueJS.svg' },
+      { name : 'Bootstrap', icon: 'Bootstrap.svg' },
+      { name : 'TailwindCSS', icon: 'TailwindCSS.svg' },
+    ]
+  },
+    {
+    name: 'Dev Tools',
+    skills : [
+      { name : 'Git', icon: 'Git'},
+      { name : 'VSCode', icon: 'VSCode'},
+      { name : 'Jetbrains IDEs', icon: 'jetbrains.svg'},
+      { name : 'Jira', icon: 'Jira'},
+      { name : 'Firebase', icon: 'Firebase'},
+      { name : 'Docker', icon: 'Docker'},
+    ]
+  },
+]
+
+const educations = [
+  {
+    title: '🎓 Computer Science Engineering Master Degree',
+    subtitle: 'ICT engineering business and innovation',
+    place: 'Politecnico di Milano, Italy'
+  },
+    {
+    title: '🎓 Bechelor\'s Degree',
+    subtitle: 'Informatics and Automation Engineering',
+    place: 'Politecnico di Bari, Italy',
+    description: 'Graduated with 98/110'
+  },
+  {
+    title: '📚 High school Diploma',
+    subtitle: 'Administration, finance and Marketing',
+    place: 'Istituto Tecnico Colamonico, Acquaviva, Italy',
+    description: 'Graduated with 100/100 with honors'
+  },
+  {
+    title: '🇬🇧 English certification | B2 advanced',
+    subtitle: 'TOEIC English Certificate',
+    description: 'Passed with result 940/990'
+  },
+  {
+    title: '👨🏻‍💻 Oracle Certified Associate JAVA SE 5/SE 6'
+  }
+]
+
+const hobbies = [
+  ['💰','Personal finance'],
+  ['📈','Analytics & statistics'],
+  ['🤖','Conversational chatbots'],
+  ['📱','Technology and innovation'],
+  ['👨‍🍳','Cooking'],
+  ['🏀','Basket'],
+  ['🎾','Tennis'],
+  ['🏂','Snowboard'],
+]
