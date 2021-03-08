5e3315574c221905692678033c1e37a2462556d1
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2021-03-08 16:30:57 +0100

    added graphql

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const workExperiences = [
-
   {
     role: 'Frontend Developer',
     company: 'Datbrain LTD',
@@ -7,8 +6,9 @@ const workExperiences = [
     place: 'London, England (Remote)',
     works: [
       {
-        description: 'Development of Smarkez, a business browser in a cross-platform desktop app. Working in a 15+ devs team.',
-        techs: ['Angular', 'Electron', 'Bootstrap']
+        description:
+          'Development of Smarkez, a business browser in a cross-platform desktop app. Working in a 15+ devs team.',
+        techs: ['Angular', 'Electron', 'Bootstrap'],
       },
     ],
   },
@@ -20,21 +20,24 @@ const workExperiences = [
     place: 'Bari, Italy (Remote)',
     works: [
       {
-        description: 'Design and development of cloud software in continuous evolution for the management of financial movements, work shifts, calculation of salaries, analysis of collected data.',
-        techs: ['Laravel', 'VueJS', 'Bootstrap']
+        description:
+          'Design and development of cloud software in continuous evolution for the management of financial movements, work shifts, calculation of salaries, analysis of collected data.',
+        techs: ['Laravel', 'VueJS', 'Bootstrap'],
       },
       {
-        description: 'Design and development of the Android app Digital Wine Showcase, currently running in production on more than 30 tablets, used as an electronic wine list in restaurants.',
-        techs: ['Angular', 'Ionic']
+        description:
+          'Design and development of the Android app Digital Wine Showcase, currently running in production on more than 30 tablets, used as an electronic wine list in restaurants.',
+        techs: ['Angular', 'Ionic'],
       },
       {
         description: 'Development of corporate group websites, including e-commerce.',
-        techs: ['Laravel', 'Bootstrap', 'Prestashop']
+        techs: ['Laravel', 'Bootstrap', 'Prestashop'],
       },
       {
-        description: 'Development of mail and Instagram automation tool, with web scraping for new customer acquisition and customer loyalty.',
-        techs: ['Python']
-      }
+        description:
+          'Development of mail and Instagram automation tool, with web scraping for new customer acquisition and customer loyalty.',
+        techs: ['Python'],
+      },
     ],
   },
 
@@ -45,8 +48,9 @@ const workExperiences = [
     place: 'Bari, Italy (Remote)',
     works: [
       {
-        description: 'Web app design and development for the management of short-term products. It’s currently running in the kitchens of burger king branch in Bari, Italy.',
-        techs: ['Laravel', 'VueJS', 'Bootstrap']
+        description:
+          'Web app design and development for the management of short-term products. It’s currently running in the kitchens of burger king branch in Bari, Italy.',
+        techs: ['Laravel', 'VueJS', 'Bootstrap'],
       },
     ],
   },
@@ -58,8 +62,9 @@ const workExperiences = [
     place: 'Acquaviva d. Fonti, Italy',
     works: [
       {
-        description: 'Coach of the Laravel PHP framework for the company iCommLab\'s employees in a intensive on-site 32 hours course.',
-        techs: ['Laravel']
+        description:
+          "Coach of the Laravel PHP framework for the company iCommLab's employees in a intensive on-site 32 hours course.",
+        techs: ['Laravel'],
       },
     ],
   },
@@ -71,89 +76,91 @@ const workExperiences = [
     place: 'Rome, Italy',
     works: [
       {
-        description: 'Admap is a startup I co-founded in 2015. It is an online platform that allows businesses to search available physical advertising spaces (like posters, billboards) on a map and to buy them online. In January 2017 Admap was acquired by a private company based in Rome.',
-        techs: ['Laravel', 'Python', 'Bootstrap']
+        description:
+          'Admap is a startup I co-founded in 2015. It is an online platform that allows businesses to search available physical advertising spaces (like posters, billboards) on a map and to buy them online. In January 2017 Admap was acquired by a private company based in Rome.',
+        techs: ['Laravel', 'Python', 'Bootstrap'],
       },
     ],
   },
-]
+];
 
 const skillCategories = [
   {
     name: 'Backend',
-    skills : [
+    skills: [
       { name: 'PHP', icon: 'php.svg' },
       { name: 'Laravel', icon: 'laravel.svg' },
       { name: 'Python', icon: 'python.svg' },
       { name: 'Django', icon: 'django.svg' },
-      { name: 'MySQL', icon: 'mysql.svg' }
-    ]
+      { name: 'MySQL', icon: 'mysql.svg' },
+      { name: 'GraphQL', icon: 'graphql.svg' },
+    ],
   },
   {
     name: 'Frontend',
-    skills : [
-      { name : 'HTML', icon: 'html.svg' },
-      { name : 'CSS', icon: 'css.svg' },
-      { name : 'Javascript', icon: 'javascript.svg' },
-      { name : 'Sass', icon: 'sass.svg' },
-      { name : 'Typescript', icon: 'typescript.svg' },
-      { name : 'Angular', icon: 'angular.svg' },
-      { name : 'Electron', icon: 'electron.svg' },
-      { name : 'Ionic', icon: 'ionic.svg' },
-      { name : 'VueJS', icon: 'vuejs.svg' },
-      { name : 'Bootstrap', icon: 'bootstrap.svg' },
-      { name : 'Tailwind CSS', icon: 'tailwindcss.svg' },
-    ]
+    skills: [
+      { name: 'HTML', icon: 'html.svg' },
+      { name: 'CSS', icon: 'css.svg' },
+      { name: 'Javascript', icon: 'javascript.svg' },
+      { name: 'Sass', icon: 'sass.svg' },
+      { name: 'Typescript', icon: 'typescript.svg' },
+      { name: 'Angular', icon: 'angular.svg' },
+      { name: 'Electron', icon: 'electron.svg' },
+      { name: 'Ionic', icon: 'ionic.svg' },
+      { name: 'VueJS', icon: 'vuejs.svg' },
+      { name: 'Bootstrap', icon: 'bootstrap.svg' },
+      { name: 'Tailwind CSS', icon: 'tailwindcss.svg' },
+    ],
   },
-    {
+  {
     name: 'Dev Tools',
-    skills : [
-      { name : 'Git', icon: 'git.svg'},
-      { name : 'VSCode', icon: 'vscode.svg'},
-      { name : 'Jetbrains', icon: 'phpstorm.svg'},
-      { name : 'Jira', icon: 'jira.svg'},
-      { name : 'Firebase', icon: 'firebase.svg'},
-      { name : 'Docker', icon: 'docker.svg'},
-    ]
+    skills: [
+      { name: 'Git', icon: 'git.svg' },
+      { name: 'VSCode', icon: 'vscode.svg' },
+      { name: 'Jetbrains', icon: 'phpstorm.svg' },
+      { name: 'Jira', icon: 'jira.svg' },
+      { name: 'Firebase', icon: 'firebase.svg' },
+      { name: 'Docker', icon: 'docker.svg' },
+    ],
   },
-]
+];
 
 const educations = [
   {
     title: '🎓 Computer Science Engineering Master Degree',
     subtitle: 'ICT engineering business and innovation',
     place: 'Politecnico di Milano, Italy',
-    description: 'In progress'
+    description: 'In progress',
   },
-    {
-    title: '🎓 Bachelor\'s Degree',
+  {
+    title: "🎓 Bachelor's Degree",
     subtitle: 'Informatics and Automation Engineering',
     place: 'Politecnico di Bari, Italy',
-    description: '98/110'
+    description: '98/110',
   },
   {
     title: '📚 High school Diploma',
     subtitle: 'Administration, finance and Marketing',
     place: 'Colamonico Technical Inst., Acquaviva, Italy',
-    description: '100/100 + honors'
+    description: '100/100 + honors',
   },
   {
     title: '🇬🇧 English certification | B2 advanced',
     subtitle: 'TOEIC English Certificate',
-    description: '940/990'
+    description: '940/990',
   },
   {
-    title: '👨🏻‍💻 Oracle Certified Associate JAVA SE 5/SE 6'
-  }
-]
+    title: '👨🏻‍💻 Oracle Certified Associate JAVA SE 5/SE 6',
+  },
+];
 
 const hobbies = [
-  ['💰','Personal finance'],
-  ['📈','Analytics & statistics'],
-  ['🤖','Conversational chatbots'],
-  ['🖥','Technology and innovation'],
-  ['👨‍🍳','Cooking'],
-  ['🏀','Basket'],
-  ['🎾','Tennis'],
-  ['🏂','Snowboard'],
-]
+  ['💰', 'Personal finance'],
+  ['📈', 'Analytics & statistics'],
+  ['🤖', 'Conversational chatbots'],
+  ['🖥', 'Technology and innovation'],
+  ['👨‍🍳', 'Cooking'],
+  ['🏀', 'Basket'],
+  ['🎾', 'Tennis'],
+  ['🏂', 'Snowboard'],
+];
